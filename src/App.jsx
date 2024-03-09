d200976cfee5931fefd0cd34b0d2779437a49e40
srxvathsav <sreevathsav2002@gmail.com>
2024-03-09 23:46:42 +0530

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from "./Navbar.jsx";
+
 function App() {
   return (
     <>
-      <h1 className=" text-center text-3xl font-bold underline">Trim Time !</h1>
+      <Navbar />
     </>
   );
 }
